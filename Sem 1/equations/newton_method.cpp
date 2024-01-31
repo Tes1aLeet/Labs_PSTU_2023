17d28908ea47d96fa0077b8b4af833d2451985c0
Tes1aLeet <43112134+Tes1aLeet@users.noreply.github.com>
2024-01-31 13:26:41 +0000

    Update newton_method.cpp

--- Sem 1/equations/newton_method.cpp
+++ Sem 1/equations/newton_method.cpp
@@ -5,12 +5,12 @@ using namespace std;
 
 double get_value(double x)
 {
-
+  // y = 3sin(x) * sqrt(x) + 0.35x - 3.8
   return ( 3 * sin(x) * sqrt(x) ) + ( 0.35 * x ) - 3.8;
 }
 double get_derivative(double x)
 {
-
+  // y'= (1.5sin(x) + xcos(x)) / sqrt(x) + 0.35
   return ( ( 1.5 * sin(x) ) + ( x * cos(x) ) ) / sqrt(x) + 0.35;
 }
 
