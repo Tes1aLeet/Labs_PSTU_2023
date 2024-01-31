d4eed59c1b9ff49b2dd0680c54aa264caf897a11
Tes1aLeet <43112134+Tes1aLeet@users.noreply.github.com>
2024-01-31 21:57:53 +0000

    Add files via upload

--- Sem 2/3_4/3_4.cpp
+++ Sem 2/3_4/3_4.cpp
@@ -1,1 +1,31 @@
+#include <iostream>
 
+using namespace std;
+
+int main()
+{
+  const int SIZE = 5;
+
+  bool organized = true;
+
+  int a[SIZE] = {1,2,3,4,5};
+
+  for (int i = 0; i < SIZE - 1; i++)
+  {
+    if (a[i] >= a[i + 1])
+    {
+      organized = false;
+      break;
+    }
+  }
+  if (organized)
+  {
+    cout << "массив упорядочен по возрастанию" << endl;
+  }
+  else
+  {
+    cout << "массив не упороядочен по возрастанию" << endl;
+  }
+
+  return 0;
+}
