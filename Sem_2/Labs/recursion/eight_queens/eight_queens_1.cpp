c5cd0f15dbdcdd5922e58c2e61caa3aad4fb2a0b
Tes1aLeet <dipanda123456@gmail.com>
2024-02-12 03:20:56 +0000

    изменено:      eight_queens_1.cpp

--- Sem_2/Labs/recursion/eight_queens/eight_queens_1.cpp
+++ Sem_2/Labs/recursion/eight_queens/eight_queens_1.cpp
@@ -7,15 +7,15 @@ void print(int* a, const int size);
 
 bool conflicts(int* a, int pos, const int size);
 
-bool place_queens(int* a, int current, const int size);
+bool place_queens(int* a, int current_line, const int size);
 
 int main()
 {
-  const int SIZE = 8;
+  const int SIZE = 2;
 
   // массив позиций ферзей (индекс - номер строки, значение - позиция (порядковый номер - 1, если считать фигуры слева направо сверху вниз) ) 
-  // всегда должен быть упорядочен от меньшего к большему (от самого маленького значения позиции к самому большому)
-  // если значение = -1, то клетка пустая
+  // массив всегда должен быть упорядочен от меньшего к большему (от самого маленького значения позиции к самому большому)
+  // если значение = -1, то клетка пустая, все клетки после нее не обрабатываются
   int queens[SIZE] = {};
 
   for(int i = 1; i < SIZE; i++)
@@ -25,10 +25,12 @@ int main()
 
   srand(time(NULL));
   queens[0] = rand() % SIZE;
+  
 
-  place_queens(queens, SIZE - 1, SIZE);
-
-  print(queens, SIZE);
+  if(!place_queens(queens, 1, SIZE))
+  {
+    cout << "Решения не существует" << endl;
+  }
 
   return 0;
 }
@@ -43,12 +45,12 @@ void print(int* a, const int size)
     {
       if(a[current_queen] == j + (i * size) )
       {
-        cout << '*' << ' ';
+        cout << '@' << ' ';
         current_queen++;
       }
       else
       {
-        cout << '_' << ' ';
+        cout << '-' << ' ';
       }
     }
     cout << endl;
@@ -82,45 +84,46 @@ bool conflicts(int* a, int pos, const int size)
 
     if(a_row == row || a_col == col) // если в одной строке или в одном столбце
     {
-      cout << "conflict: (" << a_row + 1 << "; " << a_col + 1 << ") and (" << row + 1 << "; " << col + 1 << ") a_pos: " << a_pos << " pos: " << pos << endl;
       return true;
     }
     if(a_col - col == a_row - row || a_col - col == row - a_row)  // если на одной диагонали
     {
-      cout << "conflict: (" << a_row + 1 << "; " << a_col + 1 << ") and (" << row + 1 << "; " << col + 1 << ") a_pos: " << a_pos << " pos: " << pos << endl;
       return true;
     }
   }
-
   return false;
 }
 
-
+// true:  если с текущей расстановкой ферзей возможно заполнить все строки до current_line
+// false: если невозможно
 bool place_queens(int* a, int current_line, const int size)
 {
-  if(current_line == 0)
+  if(current_line >= size) // если дошли до самой нижней
   {
     return true;
   }
   
+  // i - номер колонны, в которую пробуем ставить ферзя
   for(int i = 0; i < size; i++)
   {
-    if(place_queens(a, current_line - 1, size)) 
+    // если в точке (current_line + 1, i + 1) на доске можно поставить ферзя
+    if(!conflicts(a, (current_line * size) + i, size))
     {
-      if(!conflicts(a, (current_line * size) + i, size))
+      // то ставим ферзя
+      a[current_line] = i + (current_line * size);
+      print(a, size);
+
+      // если при текущей расстановке ферзей, можно расставить все оставшиеся ферзи
+      if(place_queens(a, current_line + 1, size))
       {
-        a[current_line] = (current_line * size) + i;
-        return true;
+        // то оставляем все как есть
+        return true;    
       }
-    }
-    else
-    {
-      a[(current_line * size) + i] = -1;
+      
+      // если нет - то, убираем ферзя в текущей клетке, и пробуем следующую
+      a[current_line] = -1;
     }
   }
 
   return false;
 }
-
-
-// доходим до самой нижней строки
